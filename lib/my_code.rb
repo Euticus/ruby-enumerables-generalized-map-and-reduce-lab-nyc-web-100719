7daa5981f0af2b7625da3a45c096c7861ebe74a7
Jay  Stewart <jayr.stewart@live.com>
2019-09-28 16:35:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,5 @@
 # Your Code Here
 def my_own_map (array)
-  yield (array).map{ |i| i*-1 }
-  yield (array).map{ |i| i }
-  yield (array).map{ |i| i*2 }
-  yield (array).map{ |i| i**2 }
+  array.map
 end
 
