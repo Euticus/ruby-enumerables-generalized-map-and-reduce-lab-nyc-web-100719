030a2f44a369b95a3496b89eba36acaddc27add7
Jay  Stewart <jayr.stewart@live.com>
2019-09-28 16:15:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,10 @@
 # Your Code Here
 def my_own_map (array)
   array.map{ 
-    yield
+    yield array[i];
+    yield array[i] * -1; 
+    yield array[i] **2;
+    yield array[i]*2;
   }
 end
 
-my_own_map do |array| 
-   array[i];
-   array[i] * -1; 
-   array[i] **2;
-   array[i]*2;
-  end 
