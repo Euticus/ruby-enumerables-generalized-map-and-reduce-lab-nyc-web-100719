5683c0a747668b9a30bb3a0fc0b00196a4f7e74c
Jay  Stewart <jayr.stewart@live.com>
2019-09-28 16:11:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,8 +6,8 @@ def my_own_map (array)
 end
 
 my_own_map do |array| 
-   array[i], 
-   array[i] * -1, 
-   array[i] **2, 
-   array[i]*2, 
+   array[i];
+   array[i] * -1; 
+   array[i] **2;
+   array[i]*2;
   end 
